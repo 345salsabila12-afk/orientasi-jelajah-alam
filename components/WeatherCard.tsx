55eb601ee335a6fb48896c6c9c3b84c3b00aba8f
Nilna salsabila <60324006@mhs.uingusdur.ac.id>
2026-09-15 15:22:42 +0700

    Update project

--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -6,9 +6,9 @@ export default function weatheCard({ kota, suhu, tingkatAQI }: WeatherCardProps)
   const warnaAQI = tingkatAQI === "BAIK" ? "green" : "orange";
   return (
     <View style={{ padding: 16, borderRadius: 8, backgroundColor: "#F4F7FA"}}>
-        <Text style={{ fontWeight: "bold", fontSize: 18 }}>{kota}</text>
-        <Text style={{ fontSize: 32 }}>{suhu}°C</text>
-        <Text style={{ color: warnaAQI }}>AQI: {tingkatAQI}</text>
+        <Text style={{ fontWeight: "bold", fontSize: 18 }}>{kota}</Text>
+        <Text style={{ fontSize: 32 }}>{suhu}°C</Text>
+        <Text style={{ color: warnaAQI }}>AQI: {tingkatAQI}</Text>
     </View>
   );
 }
